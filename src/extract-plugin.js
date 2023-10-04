8639dc528e82a976b78a546a57b32129bcc153d1
Holmes Conan <holmesconan@gmail.com>
2023-10-04 13:51:56 +0800

    extract CleverNote theme.

--- src/extract-plugin.js
+++ src/extract-plugin.js
@@ -24,10 +24,10 @@ function readTiddler(filename) {
       fields.text = '\n'
     }
   })
-  return {
-    ...fields,
-    text: fields.text.trim()
+  if (fields.text) {
+    fields.text = fields.text.trim()
   }
+  return fields
 }
 
 /**
@@ -36,55 +36,82 @@ function readTiddler(filename) {
  * @param {String} title 
  * @param {Object} meta 
  */
-function tiddlerFileName(dstPath, title, meta) {
+function tiddlerFilePath(dstPath, title, meta) {
   let filename
-  if (title.indexOf(meta.title) === 0) {
-    filename = title.from(meta.title.length)
-
+  if (title.indexOf(meta.prefix) === 0) {
+    filename = title.substr(meta.prefix.length)
   } else {
     filename = title.replace('$:/', '')
   }
-  filename = filename.replace('/', '_')
+  if (filename[0] === '/') {
+    filename = filename.substr(1)
+  }
+
+  filename = filename.replaceAll('/', '_')
   return path.join(dstPath, filename + '.tid')
 }
 
 function writeTiddlers(dstPath, tiddlers, meta) {
   Object.values(tiddlers).forEach(fields => {
-    let content = Object.entries(fields).map(([k, v]) => k === 'text' ? '' : `${k}: ${v}`).join('\n')
-    content = `${content}\n\n${tiddler.text}`
+    let content = ''
+    Object.keys(fields).forEach(key => {
+      if (key !== 'text') {
+        content += `${key}: ${fields[key]}\n`
+      }
+    })
+    content = `${content}\n${fields.text}`
 
     const filePath = tiddlerFilePath(dstPath, fields.title, meta)
-    fs.writeFileSync(filePath, content)
+    fs.writeFile(filePath, content, (err) => {
+      const filename = path.basename(filePath)
+      if (err) {
+        console.log(`Error: write ${filename} failed: ${err}`)
+        process.exit(-2)
+      }
+      console.log(`>>> write ${filename} done`)
+    })
   })
 }
 
-function writePlginInfo(dstPath, meta) {
-  const knownFields = []
-  const info = {}
-  const exclude = ['created', 'modified', 'type', 'text']
-  Object.keys(meta).forEach(key => {
-    if (!exclude.includes(key)) {
-      info[key] = meta[key]
+function writePluginInfo(dstPath, meta) {
+  const knownFields = [
+    'title', 'name', 'plugin-type', 'core-version', 'author',
+    'description', 'dependents', 'version'
+  ]
+  let content = ''
+  knownFields.forEach((key) => {
+    if (meta[key]) {
+      const prefix = content === '' ? '  ' : ', '
+      content += `${prefix}"${key}": ${JSON.stringify(meta[key])}\n`
     }
   })
+  content = `{\n${content}}`
   const filePath = path.join(dstPath, 'plugin.info')
-  fs.writeFileSync(filePath, JSON.stringify(info, null, 2))
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      console.log(`Error: write plugin.info failed: ${err}`)
+      process.exit(-1)
+    }
+    console.log(`>>> write plugin.info in ${dstPath} done`)
+  })
 }
 
 function handler (argv) {
   const srcPath = path.resolve(__dirname, '../exp/tiddlers')
   const dstPath = path.resolve(argv.pluginPath)
   const data = readJson(path.resolve(srcPath, argv.pluginName) + '.json')
+  let meta, tiddlers
 
   if (data.tiddlers) {
-    const meta = readTiddler(path.resolve(srcPath, argv.pluginName) + '.json.meta')
-    writePluginInfo(dstPath, meta)
-    writeTiddlers(dstPath, data.tiddlers, meta)
+    tiddlers = data.tiddlers
+    meta = readTiddler(path.resolve(srcPath, argv.pluginName) + '.json.meta')
   } else {
-    const tiddlers = JSON.parse(data[0].text).tiddlers
-    writePlginInfo(dstPath, data[0])
-    writeTiddlers(dstPath, tiddlers, data[0])
+    tiddlers = JSON.parse(data[0].text).tiddlers
+    meta = data[0]
   }
+  meta.prefix = argv.pluginPrefix || meta.title
+  writePluginInfo(dstPath, meta)
+  writeTiddlers(dstPath, tiddlers, meta)
 }
 
 module.exports = {
@@ -97,6 +124,9 @@ module.exports = {
     }).positional('plugin-path', {
       describe: 'the target path to save plugin',
       type: 'string'
+    }).option('plugin-prefix', {
+      type: 'string',
+      describe: 'the prefix for plugin title'
     })
   },
   handler
